fc4b4c3ec1a70fc016c88b69cf7dbcf08456f22f
Mahmoud Magdy <mahmoud.magdy1255@gmail.com>
2019-08-02 21:07:32 -0700

    Sss

--- routes/admin.php
+++ routes/admin.php
@@ -17,7 +17,6 @@ Route::group(['prefix' => 'admin'], function () {
 		Route::get('/admin/reports', 'AdminController@problems');
 		Route::resource('/admin', 'AdminController');
 		Route::get('/admin', 'AdminController@search')->name('admin.index');
-		Route::get('/admin', 'AdminController@search')->name('admin.index');
 
 	});
 
